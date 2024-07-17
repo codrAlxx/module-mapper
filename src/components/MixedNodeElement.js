0b30bc883327f3154b125e0e78857b9407ae542a
Tekriwal <antekriwal@deloitte.com>
2024-07-17 18:34:22 +0530

    Modified frontend colors and overall styling of the tree view

--- src/components/MixedNodeElement.js
+++ src/components/MixedNodeElement.js
@@ -2,17 +2,19 @@ import React, { useState, useRef } from 'react';
 import { Space, Card } from 'antd';
 
 const CardComp = ({nodeData={}}) => (
-  <Space direction="vertical" size={16}>
-    <Card
+  <Space direction="vertical" size={16} style={{color: "white"}}>
+    <Card titleStyle={{ background: '#ff0000' , color: "white"}}
       hoverable
-      title={nodeData.attributes.Type}
-      extra={nodeData.name}
+      title={<span style={{color: "white"}}>{nodeData.attributes.Type}</span>}
+      extra={<span style={{color: "white"}}>{nodeData.name}</span>}
       style={{
-        width: 300,
-        backgroundColor: '#7284a7',
+        height: 300,
+        width: 400,
+        backgroundColor: '#7278ff',
         color: 'black',
         overflow: 'auto',
         zIndex: 1,
+        color: "white",
         position: 'relative'
       }}
     >
@@ -40,33 +42,33 @@ const MixedNodeElement = ({ nodeData = {}, triggerNodeToggle, foreignObjectProps
   };
 
   const handleMouseLeave = () => {
-    setIsHovering(false);
-    // timerRef.current = setTimeout(() => {
-    //   setIsHovering(false);
-    // }, 3000);
+    // setIsHovering(false);
+    timerRef.current = setTimeout(() => {
+      setIsHovering(false);
+    }, 500);
   };
 
   return (
     <React.Fragment>
-      <circle
-        r={20}
+      <circle style={{zIndex: -200}}
+        r={25}
         onMouseEnter={() => handleCircleHover(true)}
         onMouseLeave={handleMouseLeave}
         onClick={triggerNodeToggle}
       ></circle>
 
 
-      <foreignObject {...foreignObjectProps}>
+      <foreignObject {...foreignObjectProps} style={{zIndex: 100}}>
         <div
             style={{
               alignItems: 'center',
               paddingBottom: '1rem',
               width: '12vw',
-              zIndex: 2,
-              position: 'relative'
+              zIndex: 122,
+              position: 'absolute'
             }}
           >
-              {isHovering ? <CardComp nodeData={nodeData} />: <h6>{nodeData.name}</h6>} 
+              {isHovering ? <CardComp nodeData={nodeData} />: <h6 style={{color: "#444", fontWeight: 500, fontSize:  "20px"}}>{nodeData.name}</h6>} 
               
         </div>
 
