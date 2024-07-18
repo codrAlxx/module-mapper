d0abee13472a3a08e8a7f7cf93e3153bd87ae4b0
Jha <aljha@deloitte.com>
2024-07-18 16:36:30 +0530

    Message

--- src/components/ButtonComp.jsx
+++ src/components/ButtonComp.jsx
@@ -1,47 +1,33 @@
-import './BaseComp.scss'
 import React from 'react';
+import './BaseComp.scss';
+
 class Button extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        buttonText: 'Class Diagram',
-        class: ''
-      }
-    }
-    
-    handlePush = (e) => {
-      if(this.state.class === "success"){
-        const fakeDataCall = new Promise((resolve, reject) => {
-            setTimeout(function(){
-              resolve("success");
-            }, 3000);
-          });
-          this.setState({class: 'loading'}, () => {
-            fakeDataCall.then(() => {
-              this.setState({class: "success", buttonText: "Class"});
-            });
-          });
-      }
-      const fakeDataCall = new Promise((resolve, reject) => {
-        setTimeout(function(){
-          resolve("success");
-        }, 3000);
-      });
-      this.setState({class: 'loading'}, () => {
-        fakeDataCall.then(() => {
-          this.setState({class: "success", buttonText: "Tree Diagram"});
-        });
-      });
-    }
-    render() {
-      return (
-        <div className='float-button'>
-            <button onClick={this.handlePush} class={this.state.class}>
-          <span>{this.state.buttonText}</span>
-        </button>
-        </div>
-      )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      buttonText: 'Class Diagram',
+      class: ''
+    };
   }
 
-  export default Button;
+  handlePush = (e) => {
+    const buttonText = this.state.buttonText === 'Class Diagram' ? 'Tree Diagram' : 'Class Diagram';
+    this.setState({ class: 'loading' }, () => {
+      setTimeout(() => {
+        this.setState({ class: 'success', buttonText });
+      }, 3000);
+    });
+  };
+
+  render() {
+    return (
+      <div className='float-button'>
+        <button onClick={this.handlePush} className={this.state.class}>
+          <span>{this.state.buttonText}</span>
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Button;
