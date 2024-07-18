f1712515895ddb4f41c985ceac90d8ba27f43a2c
Tekriwal <antekriwal@deloitte.com>
2024-07-19 02:49:14 +0530

    Added styling to landing page and main view floating button text and gave colors to loader. Updated conditional redirection based status code of api.

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,62 +1,63 @@
 import React, { useState } from 'react';
-import { IoSend } from "react-icons/io5";
-import Loader from './Loader'
+import { IoSend } from 'react-icons/io5';
+import Loader from './Loader';
 import { useNavigate } from 'react-router-dom';
-import './HomePage.scss'
+import './HomePage.scss';
 
 const FormComponent = () => {
-const [inputValue, setInputValue] = useState('');
-const [isLoading, setIsLoading] = useState(false);
-const navigate = useNavigate();
+  const [inputValue, setInputValue] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
 
-const handleInputChange = (e) => {
+  const handleInputChange = e => {
     setInputValue(e.target.value);
-};
+  };
 
-const handleButtonClick = () => {
+  const handleButtonClick = () => {
     setIsLoading(true);
-    console.log(inputValue)
+    console.log(inputValue);
     fetch(`http://localhost:8090/process-all/?dir_path=${inputValue}`)
-    .then(response => response.json())
-    .then(() => {
+      .then(response => {
+        response.json();
         setIsLoading(false);
-        console.log("Data fetched successfully");
-        navigate('/tree'); // Navigate to /tree path
-    })
-    .catch(error => {
-        setIsLoading(false);
-        console.error("Error fetching data: ", error);
-        navigate('/tree'); 
-    });
-};
-
-return (
-    <>
-        {isLoading ?  
-            <Loader /> :
-
-            <div>
-                <div class="section full-height over-hide">	
-                    <h1>REPO MAPPER</h1>
-	            </div>	
-                <div className="form">
-                    <input 
-                        type="text" 
-                        className="form__input" 
-                        placeholder="Enter Github Repo or File Path......"
-                        value={inputValue}
-                        onChange={handleInputChange}
-                    />                     
-                    <div className="btn btn__primary" onClick={handleButtonClick}>
-                        <IoSend className="icon-arrow"/>
-                    </div>
-                </div>
-            </div>
-
-
+        if (response.status === 400) {
+        } else {
+          console.log('Data fetched successfully');
+          navigate('/tree');
         }
+      })
+      .catch(error => {
+        setIsLoading(false);
+        console.error('Error fetching data: ', error);
+        // navigate('/tree');
+      });
+  };
+
+  return (
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          <div class="project-title-div">
+            <h1 id="project-title">#Repo_Mapper</h1>
+          </div>
+          <div className="dir_input_div">
+            <input
+              type="text"
+              className="dir_input"
+              placeholder="Github Repo Link / Local File Path"
+              value={inputValue}
+              onChange={handleInputChange}
+            />
+            <div className="hit-api-button" onClick={handleButtonClick}>
+              <IoSend className="icon-arrow" />
+            </div>
+          </div>
+        </div>
+      )}
     </>
-);
+  );
 };
 
 export default FormComponent;
